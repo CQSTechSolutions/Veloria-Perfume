f7c4214b0d81cf7755716493b37d8a5c68ff51b3
Aakash Pal <akashpal1482@gmail.com>
2025-02-04 18:56:29 +0530

    Refactor home page structure; create HomePage component and update layout

--- veloria-perfume/app/page.js
+++ veloria-perfume/app/page.js
@@ -1,51 +1,7 @@
-import Image from "next/image";
+import HomePage from "./pages/HomePage";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Veloria Perfume</h1>
-      <Image
-        src="https://rukminim2.flixcart.com/image/750/900/xif0q/shopsy-perfume/e/d/r/50-black-panton-1pc-spray-perfume-50-ml-unisex-premium-quality-original-imagqwuwzajvfq3t.jpeg?q=20&crop=false"
-        alt="Veloria Perfume"
-        width={500}
-        height={500}
-      />
-
-      <p className="text-4xl text-center">
-        Veloria Perfume is a perfume brand that offers a wide range of perfumes
-        for
-        <ul
-          style={{
-            listStyleType: "none",
-            display: "flex",
-            justifyContent: "center",
-          }}
-        >
-          <li
-            style={{
-              padding: "10px",
-              border: "1px solid black",
-              margin: "10px",
-            }}
-          >
-
-
-            <Image  src="https://maherolfactive.com/cdn/shop/files/Velouriawhite_530x@2x.jpg?v=1685406879" alt="Perfume 1" width={100} height={100} />
-
-
-          </li>
-          <li
-            style={{
-              padding: "10px",
-              border: "1px solid black",
-              margin: "10px",
-            }}
-          >
-            <Image  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFpeW6eqNGjCtBtMXIiY-ydyW9BSyf3ULKLQ&s" alt="Perfume 2" width={100} height={100} />
-          </li>
-      
-        </ul>
-      </p>
-    </div>
+    <HomePage/>
   );
 }
--- veloria-perfume/app/pages/HomePage.js
+++ veloria-perfume/app/pages/HomePage.js
@@ -1,9 +1,15 @@
+import Link from 'next/link'
 import React from 'react'
 
 const HomePage = () => {
   return (
-    <div>
-      Home page
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold">Veloria Collections - Luxury Perfumes</h1>
+      <Link href="/products">
+        <button className="mt-5 px-6 py-3 bg-blue-600 text-white rounded-lg">
+          Explore Our Collection
+        </button>
+      </Link>
     </div>
   )
 }
